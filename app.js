6f8151b0909726bbd48af6e0184140c7e4b44be7
Toanf2103 <100524773+Toanf2103@users.noreply.github.com>
2025-08-17 21:06:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const config = {
     inviterName: "Toanf",
     movieTitle: "Cậu chọn 🤭🤭",
     cinema: "Gần cậu",
-    datetime: "2025-08-5T19:30:00+07:00",
+    datetime: "2025-08-19T19:30:00+07:00",
     seats: "H10–H11",
     contact: {
         zalo: "0523919941",
